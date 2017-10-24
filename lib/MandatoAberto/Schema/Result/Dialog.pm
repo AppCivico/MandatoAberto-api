f6b729f3db36d446a2d7cd72b1eaf6618e01ae4b
Lucas Ansei <lucas.ansei@eokoe.com>
2017-10-24 17:58:21 -0200

    Creating the dialog table

---
+++ lib/MandatoAberto/Schema/Result/Dialog.pm
@@ -0,0 +1,90 @@
+use utf8;
+package MandatoAberto::Schema::Result::Dialog;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+MandatoAberto::Schema::Result::Dialog
+
+=cut
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::TimeStamp>
+
+=item * L<DBIx::Class::PassphraseColumn>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp", "PassphraseColumn");
+
+=head1 TABLE: C<dialog>
+
+=cut
+
+__PACKAGE__->table("dialog");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+  sequence: 'dialog_id_seq'
+
+=head2 name
+
+  data_type: 'text'
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "dialog_id_seq",
+  },
+  "name",
+  { data_type => "text", is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+
+# Created by DBIx::Class::Schema::Loader v0.07047 @ 2017-10-24 17:58:03
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:4v+md/K/L4F3XH0Y9J6+Ng
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->meta->make_immutable;
+1;
