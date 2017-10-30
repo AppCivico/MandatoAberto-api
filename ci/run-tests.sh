85d14aa99dcb705090ee2a3b15bfdc73bfa1bbcd
Lucas Ansei <lucas.ansei@eokoe.com>
2017-10-30 13:45:07 -0200

    Fix: name

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -6,4 +6,4 @@ echo "doing cpanm --installdeps on $DIR"
 cpanm Module::Install::Catalyst App::Sqitch App::ForkProve -n
 cpanm -n --installdeps .
 sqitch deploy -t $1
-TRACE=1 forkprove -MDonm -j4  -lvr t/
+TRACE=1 forkprove -MMandatoAberto -j4  -lvr t/
