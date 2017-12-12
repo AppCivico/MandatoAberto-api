d3ec67ad1fafdb815148deb309f4c17c3c37622f
Lucas Ansei <lucas.ansei@eokoe.com>
2017-12-12 13:33:28 -0200

    Fix: return only citizens tied to politician

--- lib/MandatoAberto/Controller/API/Politician/Citizen.pm
+++ lib/MandatoAberto/Controller/API/Politician/Citizen.pm
@@ -7,16 +7,10 @@ use Scalar::Util qw(looks_like_number);
 BEGIN { extends "CatalystX::Eta::Controller::REST" }
 
 with "CatalystX::Eta::Controller::AutoBase";
-with "CatalystX::Eta::Controller::AutoListGET";
 
 __PACKAGE__->config(
     result  => "DB::Citizen",
     no_user => 1,
-
-    list_key => "citizens",
-    build_row  => sub {
-        return { $_[0]->get_columns() };
-    },
 );
 
 sub root : Chained('/api/politician/object') : PathPart('') : CaptureArgs(0) { }
@@ -25,7 +19,32 @@ sub base : Chained('root') : PathPart('citizen') : CaptureArgs(0) { }
 
 sub list : Chained('base') : PathPart('') : Args(0) : ActionClass('REST') { }
 
-sub list_GET { }
+sub list_GET {
+    my ($self, $c) = @_;
+
+    my $politician_id = $c->stash->{politician}->{user_id};
+
+    return $self->status_ok(
+        $c,
+        entity => {
+            citizens => [
+                map {
+                    my $c = $_;
+
+                    +{
+                        id            => $c->get_column('id'),
+                        name          => $c->get_column('name'),
+                        email         => $c->get_column('email'),
+                        cellphone     => $c->get_column('cellphone'),
+                        gender        => $c->get_column('gender'),
+                        origin_dialog => $c->get_column('origin_dialog'),
+                        created_at    => $c->get_column('created_at'),
+                    }
+                } $c->stash->{collection}->search( { politician_id => $politician_id } )->all()
+            ]
+        }
+    )
+}
 
 __PACKAGE__->meta->make_immutable;
 
