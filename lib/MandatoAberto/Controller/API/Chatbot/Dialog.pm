a02e0192088c9e7ee89055f59c1932fe08bb998e
lucas <lucas@lucastakushi.com>
2017-12-19 15:56:49 -0200

    Implementing chatbot dialog GET

---
+++ lib/MandatoAberto/Controller/API/Chatbot/Dialog.pm
@@ -0,0 +1,75 @@
+package MandatoAberto::Controller::API::Chatbot::Dialog;
+use common::sense;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'CatalystX::Eta::Controller::REST' }
+
+with "CatalystX::Eta::Controller::AutoBase";
+
+__PACKAGE__->config(
+    # AutoBase.
+    result => "DB::Dialog",
+);
+
+sub root : Chained('/api/chatbot/base') : PathPart('') : CaptureArgs(0) { }
+
+sub base : Chained('root') : PathPart('dialog') : CaptureArgs(0) {  }
+
+sub list : Chained('base') : PathPart('') : Args(0) : ActionClass('REST') { }
+
+sub list_GET {
+    my ($self, $c) = @_;
+
+    my $politician_id = $c->req->params->{politician_id};
+    die \["politician_id", "missing"] unless $politician_id;
+
+    return $self->status_ok(
+        $c,
+        entity => {
+            dialogs => [
+                map {
+                    my $d = $_;
+                    +{
+                        id          => $d->get_column('id'),
+                        name        => $d->get_column('name'),
+                        description => $d->get_column('description'),
+
+                        questions => [
+                            map {
+                                my $q = $_;
+
+                                +{
+                                    id            => $q->get_column('id'),
+                                    name          => $q->get_column('name'),
+                                    content       => $q->get_column('content'),
+                                    citizen_input => $q->get_column('citizen_input'),
+
+                                    answer =>
+                                        map {
+                                            my $a = $_;
+
+                                            +{
+                                                id      => $a->get_column('id'),
+                                                content => $a->get_column('content')
+                                            }
+                                        } $c->model("DB::Answer")->search(
+                                            {
+                                                politician_id => $politician_id,
+                                                question_id   => $q->get_column('id'),
+                                            }
+                                          )->all()
+
+                                }
+                            } $d->questions->all()
+                        ],
+                    }
+                } $c->stash->{collection}->search({}, { prefetch => [ 'questions', { 'questions' => 'answers' } ] })->all()
+            ],
+        }
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
