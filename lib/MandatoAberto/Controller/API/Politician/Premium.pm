26b559ebfb5b5266abe7c50af940c70d9a303821
Lucas Ansei <lucas.ansei@eokoe.com>
2018-01-16 11:49:09 -0200

    Implementing premium endpoint

--- lib/MandatoAberto/Controller/API/Politician/Premium.pm
+++ lib/MandatoAberto/Controller/API/Politician/Premium.pm
@@ -5,6 +5,13 @@ use namespace::autoclean;
 
 BEGIN { extends 'CatalystX::Eta::Controller::REST' }
 
+with "CatalystX::Eta::Controller::AutoBase";
+
+__PACKAGE__->config(
+    # AutoBase.
+    result => "DB::Politician",
+);
+
 sub root : Chained('/api/politician/object') : PathPart('') : CaptureArgs(0) {
     my ($self, $c) = @_;
 
@@ -16,7 +23,7 @@ sub root : Chained('/api/politician/object') : PathPart('') : CaptureArgs(0) {
 
 sub base : Chained('root') : PathPart('premium') : CaptureArgs(0) { }
 
-sub list : Chained('base') : PathPart('') : CaptureArgs(0) { }
+sub list : Chained('base') : PathPart('') : Args(0) : ActionClass('REST') { }
 
 sub list_POST {
     my ($self, $c) = @_;
@@ -24,8 +31,28 @@ sub list_POST {
     # Posteriormente será implementado um método de pagamento
     # e a renovação/contratação do serviço premium
     # será automatizada
+    my $premium = $c->req->params->{premium};
+    die \["premium", 'missing'] unless defined $premium;
+    my $politician = $c->stash->{politician}->update(
+        {
+            premium            => $premium,
+            premium_updated_at => \'NOW()'
+        }
+    );
 
-    
+    # Envio um e-mail para notificar o usuário que ele pode ou não mandar mensagens diretas
+    if ($politician->premium == 1) {
+        $c->stash->{politician}->send_premium_activated_email();
+    } else {
+        $c->stash->{politician}->send_premium_deactivated_email();
+    }
+
+    return $self->status_ok(
+        $c,
+        entity => {
+            id => $politician->id
+        }
+    );
 }
 
 __PACKAGE__->meta->make_immutable;
--- lib/MandatoAberto/Schema/Result/Politician.pm
+++ lib/MandatoAberto/Schema/Result/Politician.pm
@@ -383,7 +383,7 @@ sub verifiers_specs {
                 new_password => {
                     required => 0,
                     type     => "Str"
-                }
+                },
             }
         ),
     };
@@ -574,12 +574,196 @@ sub send_greetings_email {
     return $self->result_source->schema->resultset('EmailQueue')->create({ body => $email->as_string });
 }
 
+sub send_premium_activated_email {
+    my ($self) = @_;
+
+    my $email = MandatoAberto::Mailer::Template->new(
+        to       => $self->user->email,
+        from     => 'no-reply@mandatoaberto.com.br',
+        subject  => "Mandato Aberto - Mensagens diretas habilitadas",
+        template => get_data_section('premium-active.tt'),
+        vars     => {
+            name  => $self->name,
+        },
+    )->build_email();
+
+    return $self->result_source->schema->resultset('EmailQueue')->create({ body => $email->as_string });
+}
+
+sub send_premium_deactivated_email {
+    my ($self) = @_;
+
+    my $email = MandatoAberto::Mailer::Template->new(
+        to       => $self->user->email,
+        from     => 'no-reply@mandatoaberto.com.br',
+        subject  => "Mandato Aberto - Mensagens diretas desativadas",
+        template => get_data_section('premium-inactive.tt'),
+        vars     => {
+            name  => $self->name,
+        },
+    )->build_email();
+
+    return $self->result_source->schema->resultset('EmailQueue')->create({ body => $email->as_string });
+}
+
 __PACKAGE__->meta->make_immutable;
 1;
 
 __DATA__
 
 @@ greetings.tt
+
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<div leftmargin="0" marginheight="0" marginwidth="0" topmargin="0" style="background-color:#f5f5f5; font-family:'Montserrat',Arial,sans-serif; margin:0; padding:0; width:100%">
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="x_deviceWidth" width="600" style="border-collapse:collapse">
+<tbody>
+<tr>
+<td height="50"></td>
+</tr>
+<tr>
+<td bgcolor="#ffffff" colspan="2" style="background-color:rgb(255,255,255); border-radius:0 0 7px 7px; font-family:'Montserrat',Arial,sans-serif; font-size:13px; font-weight:normal; line-height:24px; padding:30px 0; text-align:center; vertical-align:top">
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="84%" style="border-collapse:collapse">
+<tbody>
+<tr>
+<td align="justify" style="color:#666666; font-family:'Montserrat',Arial,sans-serif; font-size:16px; font-weight:300; line-height:23px; margin:0">
+<p style="text-align: center;"><a href="https://midialibre.org.br/"><img src="https://gallery.mailchimp.com/af2df78bcac96c77cfa3aae07/images/c75c64c5-c400-4c18-9564-16b4a7116b03.png" class="x_deviceWidth" style="border-radius:7px 7px 0 0; align: center"></a></p>
+<p><b>Olá, [% name %]. </b></p>
+<br></span>
+</p>
+<p> <strong> </strong>Agradecemos seu compromisso em valorizar a imprensa. Seu apoio é fundamental para que, juntos, jornalistas e o público criem uma mídia cada vez mais livre e democrática.</p>
+<p>A partir de agora você poderá distribuir seus Libres com facilidade e segurança em toda a rede de veículos e jornalistas que utilizam nossa plataforma.</p>
+<p>Em seu perfil em nosso site, você pode acompanhar o balanço de sua conta, consultar e a lista de matérias, artigos e conteúdos que você apoiou.</p>
+<p>E fique de olho em nossos informes e atualizações. Libre é uma ferramenta nova e em constante evolução. Ao longo dos próximos meses vamos ampliar nossa rede de veículos, aprimorando o funcionamento e criando novas funcionalidades em nosso site.</p>
+<p>Qualquer dúvida procure nosso FAQ ou escreva para nós.
+<br><br>A mídia Libre conta com você!</p>
+</td>
+</tr>
+<tr>
+<td height="30"></td>
+</tr>
+<tr>
+<td align="justify" style="color:#999999; font-size:13px; font-style:normal; font-weight:normal; line-height:16px">
+<strong id="docs-internal-guid-d5013b4e-a1b5-bf39-f677-7dd0712c841b">
+<p>Dúvidas? Acesse <a href="https://midialibre.org.br/ajuda/" target="_blank" style="color:#4ab957">Perguntas frequentes</a>.</p>
+Equipe Libre
+</strong>
+<a href="mailto:contato@midialibre.org.br" target="_blank" style="color:#4ab957"></a>
+</td>
+</tr>
+<tr>
+<td height="30"></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="x_deviceWidth" width="540" style="border-collapse:collapse">
+<tbody>
+<tr>
+<td align="center" style="color:#666666; font-family:'Montserrat',Arial,sans-serif; font-size:11px; font-weight:300; line-height:16px; margin:0; padding:30px 0px">
+<span><strong>Libre</strong></span>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div></div>
+</body>
+</html>
+
+@@ premium-active.tt
+
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<div leftmargin="0" marginheight="0" marginwidth="0" topmargin="0" style="background-color:#f5f5f5; font-family:'Montserrat',Arial,sans-serif; margin:0; padding:0; width:100%">
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="x_deviceWidth" width="600" style="border-collapse:collapse">
+<tbody>
+<tr>
+<td height="50"></td>
+</tr>
+<tr>
+<td bgcolor="#ffffff" colspan="2" style="background-color:rgb(255,255,255); border-radius:0 0 7px 7px; font-family:'Montserrat',Arial,sans-serif; font-size:13px; font-weight:normal; line-height:24px; padding:30px 0; text-align:center; vertical-align:top">
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="84%" style="border-collapse:collapse">
+<tbody>
+<tr>
+<td align="justify" style="color:#666666; font-family:'Montserrat',Arial,sans-serif; font-size:16px; font-weight:300; line-height:23px; margin:0">
+<p style="text-align: center;"><a href="https://midialibre.org.br/"><img src="https://gallery.mailchimp.com/af2df78bcac96c77cfa3aae07/images/c75c64c5-c400-4c18-9564-16b4a7116b03.png" class="x_deviceWidth" style="border-radius:7px 7px 0 0; align: center"></a></p>
+<p><b>Olá, [% name %]. </b></p>
+<br></span>
+</p>
+<p> <strong> </strong>Agradecemos seu compromisso em valorizar a imprensa. Seu apoio é fundamental para que, juntos, jornalistas e o público criem uma mídia cada vez mais livre e democrática.</p>
+<p>A partir de agora você poderá distribuir seus Libres com facilidade e segurança em toda a rede de veículos e jornalistas que utilizam nossa plataforma.</p>
+<p>Em seu perfil em nosso site, você pode acompanhar o balanço de sua conta, consultar e a lista de matérias, artigos e conteúdos que você apoiou.</p>
+<p>E fique de olho em nossos informes e atualizações. Libre é uma ferramenta nova e em constante evolução. Ao longo dos próximos meses vamos ampliar nossa rede de veículos, aprimorando o funcionamento e criando novas funcionalidades em nosso site.</p>
+<p>Qualquer dúvida procure nosso FAQ ou escreva para nós.
+<br><br>A mídia Libre conta com você!</p>
+</td>
+</tr>
+<tr>
+<td height="30"></td>
+</tr>
+<tr>
+<td align="justify" style="color:#999999; font-size:13px; font-style:normal; font-weight:normal; line-height:16px">
+<strong id="docs-internal-guid-d5013b4e-a1b5-bf39-f677-7dd0712c841b">
+<p>Dúvidas? Acesse <a href="https://midialibre.org.br/ajuda/" target="_blank" style="color:#4ab957">Perguntas frequentes</a>.</p>
+Equipe Libre
+</strong>
+<a href="mailto:contato@midialibre.org.br" target="_blank" style="color:#4ab957"></a>
+</td>
+</tr>
+<tr>
+<td height="30"></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="x_deviceWidth" width="540" style="border-collapse:collapse">
+<tbody>
+<tr>
+<td align="center" style="color:#666666; font-family:'Montserrat',Arial,sans-serif; font-size:11px; font-weight:300; line-height:16px; margin:0; padding:30px 0px">
+<span><strong>Libre</strong></span>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div></div>
+</body>
+</html>
+
+@@ premium-inactive.tt
 
 <!doctype html>
 <html>
