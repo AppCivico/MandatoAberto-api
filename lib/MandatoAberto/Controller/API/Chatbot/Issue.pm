1bc5936c2df98135a723b23d054d7c4c529c6bb5
Lucas Ansei <lucas.ansei@eokoe.com>
2018-01-30 17:16:12 -0200

    Current dev state

---
+++ lib/MandatoAberto/Controller/API/Chatbot/Issue.pm
@@ -0,0 +1,41 @@
+package MandatoAberto::Controller::API::Chatbot::Issue;
+use common::sense;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'CatalystX::Eta::Controller::REST' }
+
+with "CatalystX::Eta::Controller::AutoBase";
+with "CatalystX::Eta::Controller::AutoListPOST";
+
+__PACKAGE__->config(
+    # AutoBase.
+    result => "DB::Issue",
+    no_user => 1,
+
+    prepare_params_for_create => sub {
+        my ($self, $c, $params) = @_;
+
+        my $recipient_fb_id = $c->req->params->{fb_id};
+        die \["fb_id", "missing"] unless $recipient_fb_id;
+
+        my $recipient = $c->model("DB::Recipient")->search( { fb_id => $recipient_fb_id } )->next;
+        die \["fb_id", "could not find recipient with that fb_id"] unless $recipient;
+
+        $params->{recipient_id} = $recipient->id;
+
+        return $params;
+    },
+);
+
+sub root : Chained('/api/chatbot/base') : PathPart('') : CaptureArgs(0) { }
+
+sub base : Chained('root') : PathPart('poll-result') : CaptureArgs(0) {  }
+
+sub list : Chained('base') : PathPart('') : Args(0) : ActionClass('REST') { }
+
+sub list_POST { }
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ lib/MandatoAberto/Schema/Result/Issue.pm
@@ -0,0 +1,159 @@
+use utf8;
+package MandatoAberto::Schema::Result::Issue;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+MandatoAberto::Schema::Result::Issue
+
+=cut
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::TimeStamp>
+
+=item * L<DBIx::Class::PassphraseColumn>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp", "PassphraseColumn");
+
+=head1 TABLE: C<issue>
+
+=cut
+
+__PACKAGE__->table("issue");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+  sequence: 'issue_id_seq'
+
+=head2 politician_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 recipient_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 status
+
+  data_type: 'text'
+  is_nullable: 0
+
+=head2 created_at
+
+  data_type: 'timestamp'
+  default_value: current_timestamp
+  is_nullable: 0
+  original: {default_value => \"now()"}
+
+=head2 updated_at
+
+  data_type: 'timestamp'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "issue_id_seq",
+  },
+  "politician_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "recipient_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "status",
+  { data_type => "text", is_nullable => 0 },
+  "created_at",
+  {
+    data_type     => "timestamp",
+    default_value => \"current_timestamp",
+    is_nullable   => 0,
+    original      => { default_value => \"now()" },
+  },
+  "updated_at",
+  { data_type => "timestamp", is_nullable => 1 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 politician
+
+Type: belongs_to
+
+Related object: L<MandatoAberto::Schema::Result::Politician>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "politician",
+  "MandatoAberto::Schema::Result::Politician",
+  { user_id => "politician_id" },
+  { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" },
+);
+
+=head2 recipient
+
+Type: belongs_to
+
+Related object: L<MandatoAberto::Schema::Result::Recipient>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "recipient",
+  "MandatoAberto::Schema::Result::Recipient",
+  { id => "recipient_id" },
+  { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07047 @ 2018-01-30 16:28:28
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:G5fV1wgjR7kioeDQirKObA
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->meta->make_immutable;
+1;
