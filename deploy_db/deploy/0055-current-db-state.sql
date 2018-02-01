a7ce93e7863d4d2e922286b502d5b7cc2ec46b21
Lucas Ansei <lucas.ansei@eokoe.com>
2018-02-01 14:04:28 -0200

    Fix: sqitch

--- deploy_db/deploy/0055-current-db-state.sql
+++ deploy_db/deploy/0055-current-db-state.sql
@@ -3,16 +3,6 @@
 
 BEGIN;
 
-CREATE TABLE issue (
-    id            SERIAL PRIMARY KEY,
-    politician_id INTEGER REFERENCES politician(user_id) NOT NULL,
-    recipient_id  INTEGER REFERENCES recipient(id) NOT NULL,
-    message       TEXT NOT NULL,
-    status        TEXT NOT NULL,
-    updated_at    TIMESTAMP WITHOUT TIME ZONE,
-    created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
-);
-
 ALTER TABLE issue ADD COLUMN reply TEXT;
 
 ALTER TABLE issue DROP COLUMN status;
