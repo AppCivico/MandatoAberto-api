df6ada763842ee098f4cf9e23232e557721cc3f8
lucas <lucas@lucastakushi.com>
2018-02-26 05:22:25 -0300

    Fix: Content-type on issues

--- lib/MandatoAberto/Schema/Result/Issue.pm
+++ lib/MandatoAberto/Schema/Result/Issue.pm
@@ -243,7 +243,7 @@ sub action_specs {
                 $self->_httpcb->add(
                     url     => $ENV{FB_API_URL} . '/me/messages?access_token=' . $access_token,
                     method  => "post",
-                    headers => 'Content-Type:application/json',
+                    headers => 'Content-Type: application/json',
                     body    => encode_json {
                         recipient => {
                             id => $recipient->fb_id
