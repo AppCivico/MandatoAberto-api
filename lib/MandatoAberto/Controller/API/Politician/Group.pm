9cf17f0f6dfdd3a9d40f9d42cf95ff04d78955db
Junior Moraes <carlos@eokoe.com>
2018-03-13 10:53:48 -0300

    Add total count to guide pagination
    MA-550 #time 40m #done

--- lib/MandatoAberto/Controller/API/Politician/Group.pm
+++ lib/MandatoAberto/Controller/API/Politician/Group.pm
@@ -6,7 +6,6 @@ use namespace::autoclean;
 BEGIN { extends 'CatalystX::Eta::Controller::REST' }
 
 with 'CatalystX::Eta::Controller::AutoObject';
-with 'CatalystX::Eta::Controller::AutoListGET';
 with 'CatalystX::Eta::Controller::AutoListPOST';
 with 'CatalystX::Eta::Controller::AutoResultPUT';
 
@@ -15,22 +14,6 @@ __PACKAGE__->config(
 
     object_verify_type => 'int',
     object_key         => 'group',
-
-    list_key  => 'groups',
-    build_row => sub {
-        my ($r, $self, $c) = @_;
-
-        return {
-            id               => $r->id,
-            filter           => $r->filter,
-            name             => $r->get_column('name'),
-            status           => $r->get_column('status'),
-            updated_at       => $r->get_column('updated_at'),
-            created_at       => $r->get_column('created_at'),
-            politician_id    => $r->get_column('politician_id'),
-            recipients_count => $r->get_column('recipients_count'),
-        };
-    },
 
     data_from_body => 1,
     prepare_params_for_create => sub {
@@ -64,6 +47,7 @@ sub result_GET {
 
     my $page    = $c->req->params->{page}    || 1;
     my $results = $c->req->params->{results} || 20;
+    $results    = $results <= 20 ? $results : 20;
 
     my $group = $c->stash->{group};
 
@@ -122,22 +106,41 @@ sub result_DELETE {
 
 sub list : Chained('base') : PathPart('') : Args(0) : ActionClass('REST') { }
 
-sub list_GET { }
-
-around 'list_GET' => sub {
-    my $orig = shift;
-    my $self = shift;
-    my ($c)  = @_;
+sub list_GET {
+    my ($self, $c) = @_;
 
     my $page    = $c->req->params->{page}    || 1;
     my $results = $c->req->params->{results} || 20;
+    $results    = $results <= 20 ? $results : 20;
 
-    $results = $results > 20 ? 20 : $results;
+    my $total = $c->stash->{collection}->count;
 
     $c->stash->{collection} = $c->stash->{collection}->search( {}, { page => $page, rows => $results } );
 
-    $self->$orig(@_);
-};
+    my $func = $self->config->{build_list_row} || $self->config->{build_row};
+
+    my @rows;
+    while ( my $r = $c->stash->{collection}->next() ) {
+        push @rows, {
+            id               => $r->id,
+            filter           => $r->filter,
+            name             => $r->get_column('name'),
+            status           => $r->get_column('status'),
+            updated_at       => $r->get_column('updated_at'),
+            created_at       => $r->get_column('created_at'),
+            politician_id    => $r->get_column('politician_id'),
+            recipients_count => $r->get_column('recipients_count'),
+        };
+    }
+
+    return $self->status_ok(
+        $c,
+        entity => {
+            total  => $total,
+            groups => \@rows,
+        }
+    );
+}
 
 sub list_POST { }
 
