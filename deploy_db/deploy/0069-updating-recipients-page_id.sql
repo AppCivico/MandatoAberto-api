e230429b2b8fb3b57f9844042b3d3cd1c7a6478d
Lucas Ansei <lucas.ansei@eokoe.com>
2018-03-19 11:09:16 -0300

    Updating recipients page_id

---
+++ deploy_db/deploy/0069-updating-recipients-page_id.sql
@@ -0,0 +1,9 @@
+-- Deploy mandatoaberto:0069-updating-recipients-page_id to pg
+-- requires: 0068-add-recipient-page_id
+
+BEGIN;
+
+UPDATE recipient r SET page_id = ( SELECT fb_page_id FROM politician p WHERE p.user_id = r.politician_id ) FROM politician p WHERE r.politician_id = p.user_id;
+
+
+COMMIT;
