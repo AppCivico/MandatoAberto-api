858c17037164b971952c2ebf462204645bce165a
Lucas Ansei <lucas.ansei@eokoe.com>
2018-03-20 13:28:45 -0300

    Returning 403 if api_key is expired
    MA-526 #done #time 1h (reverted from commit c53c6ec4ba13b558ef8631b2a97d391a9eac65f4)

--- lib/MandatoAberto/Controller/API.pm
+++ lib/MandatoAberto/Controller/API.pm
@@ -15,15 +15,12 @@ sub root : Chained('/') : PathPart('api') : CaptureArgs(0) {
     if (defined($api_key)) {
         my $user_session = $c->model('DB::UserSession')->search({
             api_key      => $api_key,
-            valid_until  => { '>=' => \"NOW()" },
             #valid_for_ip => $c->req->address,
         })->next;
 
         if ($user_session) {
             my $user = $c->find_user({ id => $user_session->user_id });
             $c->set_authenticated($user);
-        } else {
-            $c->forward('forbidden');
         }
     }
 }
