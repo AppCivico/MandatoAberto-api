72e57cbfda677ca3ee5c74bc61505f3894ff1238
Lucas Ansei <lucas@lucastakushi.com>
2018-05-03 15:58:51 -0300

    Deactivating voto legal integration for now

--- lib/MandatoAberto/Controller/API/Chatbot/Politician.pm
+++ lib/MandatoAberto/Controller/API/Chatbot/Politician.pm
@@ -26,14 +26,14 @@ sub list_GET {
 
     my $politician = $c->model("DB::Politician")->search( { fb_page_id => $page_id } )->next;
 
-    my $votolegal_integration = $politician->politician_votolegal_integrations->next;
-    my $votolegal_url;
+    #my $votolegal_integration = $politician->politician_votolegal_integrations->next;
+    #my $votolegal_url;
 
-    if ($votolegal_integration) {
-        $votolegal_url = $votolegal_integration->website_url;
-    } else {
-        $votolegal_url = undef
-    }
+    #if ($votolegal_integration) {
+    #    $votolegal_url = $votolegal_integration->website_url;
+    #} else {
+    #    $votolegal_url = undef
+    #}
 
     return $self->status_ok(
         $c,
@@ -47,14 +47,14 @@ sub list_GET {
                 address_city          => $p->get_column('address_city_id'),
                 address_state         => $p->get_column('address_state_id'),
                 fb_access_token       => $p->get_column('fb_page_access_token'),
-                votolegal_integration => {
-                    map {
-                        my $vl = $_;
-
-                        votolegal_username => $vl->get_column("username"),
-                        votolegal_url      => $vl->get_column("website_url")
-                    } $p->politician_votolegal_integrations->all()
-                },
+                #votolegal_integration => {
+                #    map {
+                #        my $vl = $_;
+#
+#                #        votolegal_username => $vl->get_column("username"),
+#                #        votolegal_url      => $vl->get_column("website_url")
+#                #    } $p->politician_votolegal_integrations->all()
+                #},
 
                 party         => {
                     name    => $p->party->get_column('name'),
