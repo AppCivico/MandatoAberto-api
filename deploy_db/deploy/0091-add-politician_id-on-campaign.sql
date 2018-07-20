c74b13a732c4cd6e1edd678f8f7e287132f2cfa2
Lucas Ansei <lucas.ansei@eokoe.com>
2018-07-20 13:58:56 -0300

    Fix: sqitch

--- deploy_db/deploy/0091-add-politician_id-on-campaign.sql
+++ deploy_db/deploy/0091-add-politician_id-on-campaign.sql
@@ -6,6 +6,4 @@ BEGIN;
 ALTER TABLE campaign ADD COLUMN politician_id INTEGER REFERENCES politician(user_id);
 UPDATE campaign SET politician_id = ( SELECT politician_id FROM direct_message WHERE campaign_id = campaign.id ) WHERE type_id = 1;
 UPDATE campaign SET politician_id = ( SELECT politician_id FROM poll_propagate WHERE campaign_id = campaign.id ) WHERE politician_id IS NULL;
-ALTER TABLE campaign ALTER COLUMN politician_id SET NOT NULL;
-
 COMMIT;
