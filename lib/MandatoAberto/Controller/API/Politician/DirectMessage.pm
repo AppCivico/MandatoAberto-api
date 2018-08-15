6e6dd309c7d9b12df4ef483511e335b6545808d5
Lucas Ansei <lucas.ansei@eokoe.com>
2018-08-15 08:22:29 -0300

    Updating path

--- lib/MandatoAberto/Controller/API/Politician/DirectMessage.pm
+++ lib/MandatoAberto/Controller/API/Politician/DirectMessage.pm
@@ -159,7 +159,7 @@ sub _upload_picture {
 	die \[ 'picture', 'empty file' ]    unless $upload->size > 0;
 	die \[ 'picture', 'invalid image' ] unless $mimetype =~ m{^image\/};
 
-	my $path = join "/", "votolegal", "picture", random_string(3), DateTime->now->epoch, $tempname;
+	my $path = "/votolegal/picture/" . random_string(3) . "/"  . DateTime->now->epoch . "$tempname";
 
 	my $url = $self->uploader->upload(
 		{
