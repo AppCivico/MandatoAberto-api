ebe0f36a46e23533739c431f0d6f2f22fc2db806
Lucas Ansei <lucas.ansei@eokoe.com>
2018-08-27 16:29:35 -0300

    Increasing pagination results for now

--- lib/MandatoAberto/Controller/API/Politician/Issue.pm
+++ lib/MandatoAberto/Controller/API/Politician/Issue.pm
@@ -125,7 +125,7 @@ sub list_GET {
     }
 
     my $page    = $c->req->params->{page}    || 1;
-    my $results = $c->req->params->{results} || 20;
+    my $results = $c->req->params->{results} || 1000;
 
     return $self->status_ok(
         $c,
