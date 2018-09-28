7f3ea8aaf7cd3519866ab31eb1d1fc59a707875c
Junior Moraes <carlos@eokoe.com>
2018-09-28 17:48:30 -0300

    Add politician controller

---
+++ lib/MandatoAberto/Controller/Register/Politician.pm
@@ -0,0 +1,21 @@
+package MandatoAberto::Controller::Register::Politician;
+use Mojo::Base 'Mojolicious::Controller';
+
+use MandatoAberto::Utils qw/ is_test /;
+
+sub post {
+    my $c = shift;
+
+    my $user = $c->schema->resultset('Politician')->execute(
+        $c,
+        for  => "create",
+        with => $c->req->params->to_hash,
+    );
+
+    $c->render(
+        status => 201,
+        json   => { id => $user->id }
+    );
+}
+
+1;
