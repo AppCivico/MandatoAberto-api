1a573c05e7c525c7cfd5e5bcd60214b006ecf041
Junior Moraes <carlos@eokoe.com>
2018-10-03 14:28:54 -0300

    Fix mojo schema dump

--- bin/schema.dump.sh
+++ bin/schema.dump.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -e
-if [ -d "bin" ]; then
-  cd bin;
-fi
-
 GIT_DIR=$(git rev-parse --show-toplevel)
 CWD=$(pwd)
 source $GIT_DIR/envfile.sh
-cd $GIT_DIR/bin
+cd $GIT_DIR
 
-dbicdump -o dump_directory=../../lib -o components='["TimeStamp", "PassphraseColumn"]' -o overwrite_modifications=1 -o generate_pod=1 MandatoAberto::Schema "dbi:Pg:dbname=${POSTGRESQL_DBNAME};host=${POSTGRESQL_HOST}" $POSTGRESQL_USER $POSTGRESQL_PASSWORD
+dbicdump -Ilib -o dump_directory=./lib -o use_moose=1 -o components='["InflateColumn::DateTime", "TimeStamp", "PassphraseColumn"]' -o overwrite_modifications=1 -o generate_pod=1 MandatoAberto::Schema "dbi:Pg:dbname=${POSTGRESQL_DBNAME};host=${POSTGRESQL_HOST}" $POSTGRESQL_USER $POSTGRESQL_PASSWORD
 
 cd $CWD
