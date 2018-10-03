e868386c0ddc1ce91d4bc0e4b82332e9b404a15e
Lucas Ansei <lucas.ansei@eokoe.com>
2018-10-03 14:36:32 -0300

    Adding beggining of the 'is_role' method

--- lib/MandatoAberto/Authorization.pm
+++ lib/MandatoAberto/Authorization.pm
@@ -29,7 +29,18 @@ sub has_priv {
     return 0;
 }
 
-sub is_role    { ... }
+sub is_role    {
+    my ($self, $c, $roles) = @_;
+
+    my $user = $c->current_user;
+    if ( ref $user ) {
+        my @roles = 'ARRAY' eq ref $roles ? @{ $roles } : ($roles);
+        my @user_roles = map { $_->role->get_column('name') } $user->user_roles->all();
+
+
+    }
+}
+
 sub user_role  { ... }
 
 sub fail_render {
