0e7423e42168d83753463829ca0a1852687708fb
Junior Moraes <carlos@eokoe.com>
2018-10-03 14:43:12 -0300

    Organize routes

--- lib/MandatoAberto.pm
+++ lib/MandatoAberto.pm
@@ -1,6 +1,7 @@
 package MandatoAberto;
 use Mojo::Base 'Mojolicious';
 
+use MandatoAberto::Routes;
 use MandatoAberto::Authentication;
 use MandatoAberto::Authorization;
 use MandatoAberto::Controller;
@@ -35,38 +36,8 @@ sub startup {
     $self->helper('reply.exception' => sub { MandatoAberto::Controller::reply_exception(@_) });
     $self->helper('reply.not_found' => sub { MandatoAberto::Controller::reply_not_found(@_) });
 
-    # Router
-    my $r = $self->routes;
-
-    my $api = $r->route('/api');
-
-    # Register.
-    my $register = $api->route('/register');
-    $register->post('/politician')->to('register-politician#post');
-
-    # Login.
-    $api->post('/login')->to('login#post');
-
-    # Admin.
-    my $admin = $api->route('/admin')->over(authenticated => 1)->over(has_priv => 'admin');
-
-    # Admin::Politician
-    my $admin_politician = $admin->route('/politician');
-    $admin_politician->post('/approve')->to('admin-politician-approve#post');
-
-    # Politician.
-    my $politician_list = $api->route('/politician')->over(has_priv => ['politician', 'admin']);
-    my $politician_result = $politician_list->route('/:politician_id')->under->to('politician#stasher');
-    $politician_result->get->to('politician#get');
-    $politician_result->put->to('politician#put');
-
-    # Politician::Contact.
-    my $politician_contact = $politician_result->route('/contact')->under;
-    $politician_contact->post->to('politician-contact#post');
-
-    # Politician::Greeting.
-    my $politician_greeting = $politician_result->route('/greeting')->under;
-    $politician_greeting->post->to('politician-greeting#post');
+    # Router.
+    MandatoAberto::Routes::register($self->routes);
 }
 
 1;
---
+++ lib/MandatoAberto/Routes.pm
@@ -0,0 +1,39 @@
+package MandatoAberto::Routes;
+use strict;
+use warnings;
+
+sub register {
+    my $r = shift;
+
+    my $api = $r->route('/api');
+
+    # Register.
+    my $register = $api->route('/register');
+    $register->post('/politician')->to('register-politician#post');
+
+    # Login.
+    $api->post('/login')->to('login#post');
+
+    # Admin.
+    my $admin = $api->route('/admin')->over(authenticated => 1)->over(has_priv => 'admin');
+
+    # Admin::Politician
+    my $admin_politician = $admin->route('/politician');
+    $admin_politician->post('/approve')->to('admin-politician-approve#post');
+
+    # Politician.
+    my $politician_list = $api->route('/politician')->over(has_priv => ['politician', 'admin']);
+    my $politician_result = $politician_list->route('/:politician_id')->under->to('politician#stasher');
+    $politician_result->get->to('politician#get');
+    $politician_result->put->to('politician#put');
+
+    # Politician::Contact.
+    my $politician_contact = $politician_result->route('/contact')->under;
+    $politician_contact->post->to('politician-contact#post');
+
+    # Politician::Greeting.
+    my $politician_greeting = $politician_result->route('/greeting')->under;
+    $politician_greeting->post->to('politician-greeting#post');
+}
+
+1;
