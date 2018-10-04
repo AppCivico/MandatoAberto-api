a0848a94eea2442152940448bae2729ccd7e746e
Junior Moraes <carlos@eokoe.com>
2018-10-04 11:38:38 -0300

    Fix chatbot env helper

--- lib/MandatoAberto/Controller/Chatbot.pm
+++ lib/MandatoAberto/Controller/Chatbot.pm
@@ -1,16 +1,17 @@
 package MandatoAberto::Controller::Chatbot;
 use Mojo::Base 'MandatoAberto::Controller';
 
+use MandatoAberto::Utils;
+
 sub validade_security_token {
     my $c = shift;
 
-    my $security_token = $ENV{CHATBOT_SECURITY_TOKEN};
+    my $security_token = env('CHATBOT_SECURITY_TOKEN');
 
     if ( $security_token ne $c->req->params->to_hash->{security_token} ) {
-		$c->reply_forbidden();
-		$c->detach;
+	      $c->reply_forbidden();
+	      $c->detach;
     }
-
     return $c;
 }
 
