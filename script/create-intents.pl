76e386b74729ca5ab166ac90117df37c4cc0c5e6
Lucas Ansei <lucas.ansei@eokoe.com>
2018-10-25 10:23:34 -0300

    Adding create-intents script

---
+++ script/create-intents.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+use common::sense;
+use FindBin qw($RealBin $Script);
+use lib "$RealBin/../../lib";
+
+use MandatoAberto::SchemaConnected;
+use WebService::Dialogflow;
+
+my $df = WebService::Dialogflow->instance;
+
+use DDP; p $df;
+
+my $schema         = get_schema;
+my $entity_rs      = $schema->resultset('PoliticianEntity')->search( undef, {group_by => 'me.name'} );
+my @entitites_name = $entity_rs->get_column('name')->all();
+
+for my $intent (@entitites_name) {
+    my %intent = ( displayName => $intent );
+
+    $df->create_intent(%intent);
+}
+
+1;
