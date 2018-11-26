0223190b6649938818232f52369e654a0a3529b7
Lucas Ansei <lucas.ansei@eokoe.com>
2018-11-26 10:46:12 -0200

    Adding is_mandatoaberto

--- lib/MandatoAberto/Controller/API/Dialog.pm
+++ lib/MandatoAberto/Controller/API/Dialog.pm
@@ -92,7 +92,13 @@ sub list_GET {
                             } $d->questions->search( { 'me.active' => 1 } )->all()
                         ],
                     }
-                } $c->stash->{collection}->search( { 'me.active' => 1 }, { prefetch => [ 'questions', { 'questions' => 'answers' } ] })->all()
+                } $c->stash->{collection}->search(
+                    {
+                        'me.active'                     => 1,
+                        'organization.is_mandatoaberto' => 1
+                    },
+                    { prefetch => [ 'questions', { 'questions' => { 'answers' => { 'politician' => { 'user' => 'organization' } } } } ] }
+                  )->all()
             ],
         }
     );
