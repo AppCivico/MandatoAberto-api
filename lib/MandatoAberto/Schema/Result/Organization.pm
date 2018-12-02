cba43c3fb54c4191ec5c0b9d7241285cc5c3e3b9
Lucas Ansei <lucas.ansei@eokoe.com>
2018-12-02 16:18:36 -0200

    Adding chatbot helper

--- lib/MandatoAberto/Schema/Result/Organization.pm
+++ lib/MandatoAberto/Schema/Result/Organization.pm
@@ -184,5 +184,12 @@ __PACKAGE__->has_many(
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
+
+sub chatbot {
+    my ($self) = @_;
+
+    return $self->organization_chatbots->next;
+}
+
 __PACKAGE__->meta->make_immutable;
 1;
