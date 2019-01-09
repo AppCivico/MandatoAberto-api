87d2c476c30c41397f9f2d98f8c0273f6b100c07
Lucas Ansei <lucas.ansei@eokoe.com>
2019-01-09 15:42:25 -0200

    Updating email

--- lib/MandatoAberto/Schema/ResultSet/PoliticianEntity.pm
+++ lib/MandatoAberto/Schema/ResultSet/PoliticianEntity.pm
@@ -23,7 +23,7 @@ sub sync_dialogflow {
 		$politician_rs = $self->result_source->schema->resultset('Politician');
     }
     else {
-		$politician_rs = $self->result_source->schema->resultset('Politician')->search({ 'user.email' => { '!=' => 'appcivicotest@email.com'} },{ prefetch => 'user' });
+		$politician_rs = $self->result_source->schema->resultset('Politician')->search({ 'user.email' => 'prep_dev@email.com' },{ prefetch => 'user' });
     }
 
     my $project_id      = 'mandato-aberto-copy';
