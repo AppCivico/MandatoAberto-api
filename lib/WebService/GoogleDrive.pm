1a082209a626eb814d52e63d34580333b22306f7
Lucas Ansei <lucastakushi@hotmail.com>
2019-03-22 10:08:45 -0300

    Using envs

--- lib/WebService/GoogleDrive.pm
+++ lib/WebService/GoogleDrive.pm
@@ -10,10 +10,10 @@ has 'drive' => ( is => 'rw', lazy => 1, builder => '_build_drive' );
 
 sub _build_drive {
     Net::Google::Drive->new(
-        -client_id      => '737899973087-bahrvtg3d7e0mpd06ibt7cul8qrkre1l.apps.googleusercontent.com',
-        -client_secret  => 'rpYs0h1oaSAHI0rqRC6maFtS',
-        -access_token   => 'ya29.GlufBqM9DuuMwAXAyQspVUfeGHgbfx-6s655Uf8E-Rjw2T2m43Idka0HiHt8w3db96ozs9MSDmFz4vROu5ZircVZfv5Rvtd-N_de3sodvhodLrNt_KUjpyzTPiD3',
-        -refresh_token  => '1/7cbS8jY2U7_Onn7figWoO37YXi8VHL-v9T2PzURtrCIilPysk0YCAoXPwxCv8pq2',
+        -client_id      => $ENV{DRIVE_CLIENT_ID},
+        -client_secret  => $ENV{DRIVE_CLIENT_SECRET},
+        -access_token   => $ENV{DRIVE_ACCESS_TOKEN},
+        -refresh_token  => $ENV{DRIVE_REFRESH_TOKEN},
     );
 }
 
