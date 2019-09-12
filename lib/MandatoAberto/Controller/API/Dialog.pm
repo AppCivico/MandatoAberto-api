c9c6226a74add2ab20aed11f5a4f7e8df6e05067
Lucas Ansei <lucastakushi@hotmail.com>
2019-09-12 16:53:45 -0300

    Removing validation

--- lib/MandatoAberto/Controller/API/Dialog.pm
+++ lib/MandatoAberto/Controller/API/Dialog.pm
@@ -50,14 +50,6 @@ sub list_GET {
     my $politician_id = $c->user->id;
 
     my $politician = $c->model('DB::Politician')->find($politician_id);
-    if ( !$politician->user->organization->is_mandatoaberto ) {
-        return $self->status_ok(
-            $c,
-            entity => {
-                dialogs => []
-            }
-        );
-    }
 
     my $show_question_name = $ENV{SHOW_QUESTION_NAME};
 
