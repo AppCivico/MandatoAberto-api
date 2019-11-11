fb0afe515a7d020af99d90bcd2f09568ae4797e9
Lucas Ansei <lucas.ansei@eokoe.com>
2019-11-11 09:23:19 -0300

    Adding workaround to fix front param

--- lib/MandatoAberto/Controller/API/Politician/DirectMessage.pm
+++ lib/MandatoAberto/Controller/API/Politician/DirectMessage.pm
@@ -60,7 +60,7 @@ sub list_POST {
     my ($self, $c) = @_;
 
     my $groups;
-    if ($c->req->params->{groups}) {
+    if ($c->req->params->{groups} && length $c->req->params->{groups} > 1 ) {
         $c->req->params->{groups} =~ s/(\[|\]|(\s))//g;
 
         my @groups = split(',', $c->req->params->{groups});
