9e03c9f1bd4623d581454366db8afcc6d121db0e
Lucas Ansei <lucas.ansei@eokoe.com>
2019-11-13 10:22:06 -0300

    Fix: tmp file

--- lib/MandatoAberto/Controller/API/Politician/DirectMessage.pm
+++ lib/MandatoAberto/Controller/API/Politician/DirectMessage.pm
@@ -82,7 +82,10 @@ sub list_POST {
             # Caso seja uma campanha de email ao invés de fazer o upload do arquivo, salvo-o em uma pasta
             # Para ser utilizado no envio.
             my $attachment_folder = $ENV{CAMPAIGN_ATTACHMENT_FOLDER};
-            $upload->copy_to($attachment_folder) unless is_test or die \['file', 'failed to copy attachment'];
+
+            if (!is_test) {
+                $upload->copy_to($attachment_folder) or die \['file', 'failed to copy attachment'];
+            }
 
             my $tempname = $upload->tempname;
             $tempname    =~ s/^\/\w+\///g;
