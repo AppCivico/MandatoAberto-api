6337e49271d93faacc271d516dcc2d7143ed8686
Lucas Ansei <lucas.ansei@eokoe.com>
2019-11-27 14:29:53 -0300

    Fix: param

--- lib/MandatoAberto/Schema/Result/OrganizationTicketType.pm
+++ lib/MandatoAberto/Schema/Result/OrganizationTicketType.pm
@@ -228,7 +228,7 @@ sub action_specs {
             }
 
             if ( $values{delete_send_email_to} ) {
-                $values{delete_send_email_to} = undef;
+                $values{send_email_to} = undef;
                 delete $values{delete_send_email_to};
             }
 
