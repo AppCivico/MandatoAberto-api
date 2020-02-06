fc63a9096236b728bd7687e0e2b5cff42ed2e0c4
Lucas Ansei <lucas.ansei@eokoe.com>
2020-02-06 15:43:09 -0300

    Adding metrics GET

---
+++ lib/MandatoAberto/Controller/API/Metrics.pm
@@ -0,0 +1,43 @@
+package MandatoAberto::Controller::API::Metrics;
+use common::sense;
+use Moose;
+use namespace::autoclean;
+
+with "CatalystX::Eta::Controller::TypesValidation";
+
+BEGIN { extends 'CatalystX::Eta::Controller::REST' }
+
+sub root : Chained('/api/root') : PathPart('') : CaptureArgs(0) { }
+
+sub base : Chained('root') : PathPart('metrics') : CaptureArgs(0) { }
+
+sub list : Chained('base') : PathPart('') : Args(0) : ActionClass('REST') { }
+
+sub list_GET {
+    my ($self, $c) = @_;
+
+    $self->validate_request_params(
+        $c,
+        chatbot_id => {
+            type     => 'Int',
+            required => 1,
+        },
+        security_token => {
+            type     => 'Str',
+            required => 1
+        }
+    );
+
+    $c->req->params->{security_token} eq $ENV{METRICS_SECURITY_TOKEN}
+      or die \['security_token', 'invalid'];
+
+    my $chatbot = $c->model('DB::OrganizationChatbot')->find($c->req->params->{chatbot_id})
+      or die \['chatbot_id', 'invalid'];
+
+    return $self->status_ok(
+        $c,
+        entity => $chatbot->build_external_metrics
+    );
+}
+
+1;
